cb2ade6dc64b07f28566fbd2135c3fcf4a170f11
David Jiang <djiang86@binghamton.edu>
2018-08-08 18:26:25 +0000

    finding item with search term

--- Angular-Project/src/app/services/hero.service.ts
+++ Angular-Project/src/app/services/hero.service.ts
@@ -63,6 +63,17 @@ export class HeroService {
       );
     }
   
+  searchHeroes(term: string): Observable<Hero[]> {
+      if (!term.trim()) {
+        // if not search term, return empty hero array.
+        return of([]);
+      }
+      return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+        tap(_ => this.log(`found heroes matching "${term}"`)),
+        catchError(this.handleError<Hero[]>('searchHeroes', []))
+      );
+    }
+  
   private log(message: string) {
       this.messageService.add(`HeroService: ${message}`);
     }
