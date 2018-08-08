3a92417d00caa9743d448e40204ad25c550faec5
David Jiang <djiang86@binghamton.edu>
2018-08-08 17:06:40 +0000

    retrieving URL params

--- Angular-Project/src/app/hero-detail/hero-detail.component.ts
+++ Angular-Project/src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService }  from '../services/hero.service';
+
 import { Hero } from '../models/hero.model';
 
 @Component({
@@ -7,11 +12,22 @@ import { Hero } from '../models/hero.model';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  hero: Hero;
 
-  constructor() { }
+  constructor(
+          private route: ActivatedRoute,
+          private heroService: HeroService,
+          private location: Location        
+  ) { }
 
   ngOnInit() {
+      this.getHero();
   }
+  
+  getHero(): void {
+      const id = +this.route.snapshot.paramMap.get('id');
+      this.heroService.getHero(id)
+        .subscribe(hero => this.hero = hero);
+    }
 
 }
