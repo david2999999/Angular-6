52c032f415685e6f7b7296c4321ede2d91972219
David Jiang <djiang86@binghamton.edu>
2018-08-08 17:29:57 +0000

    retrieving data with HTTP client

--- Angular-Project/src/app/services/hero.service.ts
+++ Angular-Project/src/app/services/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Hero } from '../models/hero.model';
-import { HEROES } from '../mock/mock-heroes';
 
 import { Observable, of } from 'rxjs';
 
@@ -11,17 +11,24 @@ import { MessageService } from '../services/message.service';
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+              private http: HttpClient,
+              private messageService: MessageService
+          ) { }
   
-  getHeroes(): Observable<Hero[]> {
-      this.messageService.add('HeroService: fetched heroes');
-      return of(HEROES);
-  }
+  getHeroes (): Observable<Hero[]> {
+      return this.http.get<Hero[]>(this.heroesUrl)
+    }
   
   getHero(id: number): Observable<Hero> {
       // TODO: send the message _after_ fetching the hero
       this.messageService.add(`HeroService: fetched hero id=${id}`);
       return of(HEROES.find(hero => hero.id === id));
+    }
+  
+  private log(message: string) {
+      this.messageService.add(`HeroService: ${message}`);
     }
 }
