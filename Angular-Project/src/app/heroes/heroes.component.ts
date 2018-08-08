cb53e0bee067bb519f336981ca2e69e86afe4cd9
David Jiang <djiang86@binghamton.edu>
2018-08-08 15:16:36 +0000

    creating a list with *ngFor

--- Angular-Project/src/app/heroes/heroes.component.ts
+++ Angular-Project/src/app/heroes/heroes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../models/hero.model';
+
+import { HEROES } from '../mock/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,10 +10,7 @@ import { Hero } from '../models/hero.model';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    }
+    heroes = HEROES;
     
     constructor() { }
 
