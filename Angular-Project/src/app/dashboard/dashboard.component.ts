85a547756096436e1cefc47fa8b8fd4bf3345a79
David Jiang <djiang86@binghamton.edu>
2018-08-08 16:40:25 +0000

    created dashboard componenet

---
+++ Angular-Project/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../models/hero.model';
+import { HeroService } from '../services/hero.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
