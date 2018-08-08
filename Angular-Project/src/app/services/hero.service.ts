ae1e1f7bc23e49664cb0193a36f0dd5be3c02c0f
David Jiang <djiang86@binghamton.edu>
2018-08-08 17:59:10 +0000

    tap and catcherror in http client

--- Angular-Project/src/app/services/hero.service.ts
+++ Angular-Project/src/app/services/hero.service.ts
@@ -20,6 +20,10 @@ export class HeroService {
   
   getHeroes (): Observable<Hero[]> {
       return this.http.get<Hero[]>(this.heroesUrl)
+                      .pipe(
+                              tap(heroes => this.log('fetched heroes')),
+                              catchError(this.handleError('getHeroes', []));
+                            );
     }
   
   getHero(id: number): Observable<Hero> {
@@ -30,5 +34,19 @@ export class HeroService {
   
   private log(message: string) {
       this.messageService.add(`HeroService: ${message}`);
+    }
+  
+  private handleError<T> (operation = 'operation', result?: T) {
+      return (error: any): Observable<T> => {
+     
+        // TODO: send the error to remote logging infrastructure
+        console.error(error); // log to console instead
+     
+        // TODO: better job of transforming error for user consumption
+        this.log(`${operation} failed: ${error.message}`);
+     
+        // Let the app keep running by returning an empty result.
+        return of(result as T);
+      };
     }
 }
