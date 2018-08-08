65d28711038ead9630114cfbc809f902db739e06
David Jiang <djiang86@binghamton.edu>
2018-08-08 15:57:14 +0000

    dependency injection with services

--- Angular-Project/src/app/heroes/heroes.component.ts
+++ Angular-Project/src/app/heroes/heroes.component.ts
@@ -4,19 +4,26 @@ import { Hero } from '../models/hero.model';
 
 import { HEROES } from '../mock/mock-heroes';
 
+import { HeroService } from '../services/hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-    heroes = HEROES;
+    heroes: Hero[];
     selectedHero: Hero;
     
-    constructor() { }
+    constructor(private heroService: HeroService) { }
 
     ngOnInit() {
+        this.getHeroes();
     }
+    
+    getHeroes(): void {
+        this.heroes = this.heroService.getHeroes();
+      }
 
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
