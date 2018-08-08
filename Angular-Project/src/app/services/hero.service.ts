2a3529cef37a87aed7b41dc46401afb63c389bf9
David Jiang <djiang86@binghamton.edu>
2018-08-08 18:15:48 +0000

    added items with http POST

--- Angular-Project/src/app/services/hero.service.ts
+++ Angular-Project/src/app/services/hero.service.ts
@@ -46,6 +46,13 @@ export class HeroService {
       );
     }
   
+  addHero (hero: Hero): Observable<Hero> {
+      return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+        tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+        catchError(this.handleError<Hero>('addHero'))
+      );
+    }
+  
   private log(message: string) {
       this.messageService.add(`HeroService: ${message}`);
     }
