a10f0c96b6927e3b36611e7e65d16b01cb2adf73
David Jiang <djiang86@binghamton.edu>
2018-08-08 18:19:00 +0000

    deleting items with HTTP DELETE

--- Angular-Project/src/app/services/hero.service.ts
+++ Angular-Project/src/app/services/hero.service.ts
@@ -53,6 +53,16 @@ export class HeroService {
       );
     }
   
+  deleteHero (hero: Hero | number): Observable<Hero> {
+      const id = typeof hero === 'number' ? hero : hero.id;
+      const url = `${this.heroesUrl}/${id}`;
+
+      return this.http.delete<Hero>(url, httpOptions).pipe(
+        tap(_ => this.log(`deleted hero id=${id}`)),
+        catchError(this.handleError<Hero>('deleteHero'))
+      );
+    }
+  
   private log(message: string) {
       this.messageService.add(`HeroService: ${message}`);
     }
