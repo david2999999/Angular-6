b3cbafdac6d570350db326f432b33fb2138791c7
David Jiang <djiang86@binghamton.edu>
2018-08-08 15:00:33 +0000

    creating a class

--- Angular-Project/src/app/heroes/heroes.component.ts
+++ Angular-Project/src/app/heroes/heroes.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-    hero = 'Windstorm';
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    }
     
     constructor() { }
 
