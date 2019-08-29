b85e49fa835d27ee90ed4a7e54c6b679db74bc54
David2999999 <djiang86@binghamton.edu>
2019-08-29 09:35:11 -0400

    creating objects with constructor or object literals

---
+++ Javascript-Primer/src/WorkingWithObjects..js
@@ -0,0 +1,15 @@
+let myData = new Object();
+myData.name = "Adam";
+myData.weather = "sunny";
+
+console.log("Hello " + myData.name + ".");
+console.log("Today is " + myData.weather + ".");
+
+
+let myData = {
+  name: "Adam",
+  weather: "sunny"
+};
+
+console.log("Hello " + myData.name + ". ");
+console.log("Today is " + myData.weather + ".");
