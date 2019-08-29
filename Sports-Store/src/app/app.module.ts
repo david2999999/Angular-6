2a7fb06ac633db3169f021c8cb64a16ee6f69513
David2999999 <djiang86@binghamton.edu>
2019-08-29 15:03:50 -0400

    configuring all of the modules to root module

--- Sports-Store/src/app/app.module.ts
+++ Sports-Store/src/app/app.module.ts
@@ -2,14 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {StoreComponent} from "./store/store.component";
+import {StoreModule} from "./store/store.module";
 
 @NgModule({
   declarations: [
-    AppComponent, StoreComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, StoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
