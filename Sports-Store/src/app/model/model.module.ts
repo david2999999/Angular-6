f20fc1626eb60e902637229353130bb420edaccc
David2999999 <djiang86@binghamton.edu>
2019-09-03 09:20:53 -0400

    cart summary component"

--- Sports-Store/src/app/model/model.module.ts
+++ Sports-Store/src/app/model/model.module.ts
@@ -1,8 +1,9 @@
 import {ProductRepository} from "./product.repository";
 import {StaticDataSource} from "./static.datasource";
 import {NgModule} from "@angular/core";
+import {Cart} from "./cart.model";
 
 @NgModule({
-  providers: [ProductRepository, StaticDataSource]
+  providers: [ProductRepository, StaticDataSource, Cart]
 })
 export class ModelModule { }
--- Sports-Store/src/app/store/store.module.ts
+++ Sports-Store/src/app/store/store.module.ts
@@ -4,10 +4,11 @@ import {ModelModule} from "../model/model.module";
 import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
 import {CounterDirective} from "./counter.directive";
+import {CartSummaryComponent} from "./cartSummary/cartSummary.component";
 
 @NgModule({
   imports: [ModelModule, BrowserModule, FormsModule],
-  declarations: [StoreComponent, CounterDirective],
+  declarations: [StoreComponent, CounterDirective, CartSummaryComponent],
   exports: [StoreComponent]
 })
 export class StoreModule { }
