e8638b4932a03da2676bce902acdffa633c9222f
David2999999 <djiang86@binghamton.edu>
2019-09-04 07:43:30 -0400

    route guards

--- Sports-Store/src/app/app.module.ts
+++ Sports-Store/src/app/app.module.ts
@@ -7,6 +7,7 @@ import {RouterModule} from "@angular/router";
 import {StoreComponent} from "./store/store.component";
 import {CartDetailComponent} from "./store/cartDetail/cartDetail.component";
 import {CheckoutComponent} from "./store/checkout/checkout.component";
+import {StoreFirstGuard} from "./storeFirst.guard";
 
 @NgModule({
   declarations: [
@@ -15,12 +16,18 @@ import {CheckoutComponent} from "./store/checkout/checkout.component";
   imports: [
     BrowserModule, StoreModule,
     RouterModule.forRoot([
-        { path: "store", component: StoreComponent },
-        { path: "cart", component: CartDetailComponent },
-        { path: "checkout", component: CheckoutComponent },
+        { path: "store", component: StoreComponent ,
+          canActivate: [StoreFirstGuard]
+        },
+        { path: "cart", component: CartDetailComponent,
+          canActivate: [StoreFirstGuard]
+        },
+        { path: "checkout", component: CheckoutComponent,
+          canActivate: [StoreFirstGuard]
+        },
         { path: "**", redirectTo: "/store" }
   ])],
-  providers: [],
+  providers: [StoreFirstGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Sports-Store/src/app/store/store.component.ts
+++ Sports-Store/src/app/store/store.component.ts
@@ -2,6 +2,7 @@ import {ProductRepository} from "../model/product.repository";
 import {Component} from "@angular/core";
 import {Product} from "../model/product.model";
 import {Cart} from "../model/cart.model";
+import {Router} from "@angular/router";
 
 @Component({
   selector: "store",
@@ -12,7 +13,9 @@ export class StoreComponent {
   public productsPerPage = 4;
   public selectedPage = 1;
 
-  constructor(private repository: ProductRepository, private cart: Cart) { }
+  constructor(private repository: ProductRepository,
+              private cart: Cart,
+              private router: Router) { }
 
   get products(): Product[] {
     let pageIndex = (this.selectedPage - 1) * this.productsPerPage
@@ -50,5 +53,6 @@ export class StoreComponent {
 
   addProductToCart(product: Product) {
     this.cart.addLine(product);
+    this.router.navigateByUrl("/cart");
   }
 }
