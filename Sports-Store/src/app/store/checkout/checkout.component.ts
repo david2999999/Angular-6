9f9ee55a6dad60063be3285731d57d9efb3683ff
David2999999 <djiang86@binghamton.edu>
2019-09-05 07:39:17 -0400

    creating the checkout component

--- Sports-Store/src/app/store/checkout/checkout.component.ts
+++ Sports-Store/src/app/store/checkout/checkout.component.ts
@@ -1,6 +1,27 @@
 import { Component } from "@angular/core";
+import {OrderRepository} from "../../model/order.repository";
+import {Order} from "../../model/order.model";
+import {NgForm} from "@angular/forms";
 
 @Component({
-  template: `<div><h3 class="bg-info p-1 text-white">Checkout Component</h3></div>`
+  templateUrl: "checkout.component.html",
+  styleUrls: ["checkout.component.css"]
 })
-export class CheckoutComponent { }
+export class CheckoutComponent {
+  orderSent: boolean = false;
+  submitted: boolean = false;
+
+  constructor(public repository: OrderRepository,
+              public order: Order) {}
+
+  submitOrder(form: NgForm) {
+    this.submitted = true;
+    if (form.valid) {
+      this.repository.saveOrder(this.order).subscribe(order => {
+        this.order.clear();
+        this.orderSent = true;
+        this.submitted = false;
+      });
+    }
+  }
+}
