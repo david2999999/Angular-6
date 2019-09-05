a8593d96ed91a819531358a185dcffff71cccde4
David2999999 <djiang86@binghamton.edu>
2019-09-05 08:23:17 -0400

    retrieving data from restful service instead of dummy data

--- Sports-Store/src/app/model/model.module.ts
+++ Sports-Store/src/app/model/model.module.ts
@@ -4,8 +4,12 @@ import {NgModule} from "@angular/core";
 import {Cart} from "./cart.model";
 import {Order} from "./order.model";
 import {OrderRepository} from "./order.repository";
+import {RestDataSource} from "./rest.datasource";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
-  providers: [ProductRepository, StaticDataSource, Cart, Order, OrderRepository]
+  imports: [HttpClientModule],
+  providers: [ProductRepository, Cart, Order, OrderRepository,
+    { provide: StaticDataSource, useClass: RestDataSource }]
 })
 export class ModelModule { }
