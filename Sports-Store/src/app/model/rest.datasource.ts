489e6fa7a61f601a8dc073d5ea218b7196bb12af
David2999999 <djiang86@binghamton.edu>
2019-09-06 08:51:28 -0400

    creating http request to authenticate user

--- Sports-Store/src/app/model/rest.datasource.ts
+++ Sports-Store/src/app/model/rest.datasource.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Product} from "./product.model";
 import {Order} from "./order.model";
+import { map } from "rxjs/operators";
 
 const PROTOCOL = "http";
 const PORT = 3500;
@@ -10,6 +11,7 @@ const PORT = 3500;
 @Injectable()
 export class RestDataSource {
   baseUrl: string;
+  auth_token: string;
 
   constructor(private http: HttpClient) {
     this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
@@ -21,5 +23,14 @@ export class RestDataSource {
 
   saveOrder(order: Order): Observable<Order> {
     return this.http.post<Order>(this.baseUrl + "orders", order);
+  }
+
+  authenticate(user: string, pass: string): Observable<boolean> {
+    return this.http.post<any>(this.baseUrl + "login", {
+      name: user, password: pass
+    }).pipe(map(response => {
+      this.auth_token = response.success ? response.token : null;
+      return response.success;
+    }));
   }
 }
