e002cc375dcf6fc24f16ba1e4fb6f9fee518bd1c
David2999999 <djiang86@binghamton.edu>
2019-09-06 09:21:25 -0400

    creating a guard to protect the admin page

--- Sports-Store/src/app/model/model.module.ts
+++ Sports-Store/src/app/model/model.module.ts
@@ -6,10 +6,12 @@ import {Order} from "./order.model";
 import {OrderRepository} from "./order.repository";
 import {RestDataSource} from "./rest.datasource";
 import {HttpClientModule} from "@angular/common/http";
+import {AuthService} from "./auth.service";
 
 @NgModule({
   imports: [HttpClientModule],
   providers: [ProductRepository, Cart, Order, OrderRepository,
-    { provide: StaticDataSource, useClass: RestDataSource }]
+    { provide: StaticDataSource, useClass: RestDataSource },
+    RestDataSource, AuthService]
 })
 export class ModelModule { }
