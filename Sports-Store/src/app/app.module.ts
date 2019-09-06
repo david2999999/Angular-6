6f53f6dfee7064b697a962d7c5b37d31e71020b3
David2999999 <djiang86@binghamton.edu>
2019-09-06 08:37:21 -0400

    configure to dynamically load admin module

--- Sports-Store/src/app/app.module.ts
+++ Sports-Store/src/app/app.module.ts
@@ -25,6 +25,11 @@ import {StoreFirstGuard} from "./storeFirst.guard";
         { path: "checkout", component: CheckoutComponent,
           canActivate: [StoreFirstGuard]
         },
+        {
+          path: "admin",
+          loadChildren: "./admin/admin.module#AdminModule",
+          canActivate: [StoreFirstGuard]
+        },
         { path: "**", redirectTo: "/store" }
   ])],
   providers: [StoreFirstGuard],
