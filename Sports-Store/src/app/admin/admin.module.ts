b2ade3b68d15dc91083607810d0f42dd4c6c4fbe
David2999999 <djiang86@binghamton.edu>
2019-09-09 08:08:36 -0400

    configure routing in admin section

--- Sports-Store/src/app/admin/admin.module.ts
+++ Sports-Store/src/app/admin/admin.module.ts
@@ -11,13 +11,22 @@ import {OrderTableComponent} from "./orderTable/orderTable.component";
 
 let routing = RouterModule.forChild([
   { path: "auth", component: AuthComponent },
-  { path: "main", component: AdminComponent, canActivate: [AuthGuard]},
+  { path: "main", component: AdminComponent, canActivate: [AuthGuard],
+    children: [
+      { path: "products/:mode/:id", component: ProductEditorComponent },
+      { path: "products/:mode", component: ProductEditorComponent },
+      { path: "products", component: ProductTableComponent },
+      { path: "orders", component: OrderTableComponent },
+      { path: "**", redirectTo: "products" }
+    ]
+  },
   { path: "**", redirectTo: "auth" }
 ]);
 
 @NgModule({
   imports: [CommonModule, FormsModule, routing],
   providers: [AuthGuard],
-  declarations: [AuthComponent, AdminComponent, ProductTableComponent, ProductEditorComponent, OrderTableComponent]
+  declarations: [AuthComponent, AdminComponent,
+    ProductTableComponent, ProductEditorComponent, OrderTableComponent]
 })
 export class AdminModule { }
