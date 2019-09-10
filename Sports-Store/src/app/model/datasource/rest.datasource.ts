f35f782f60d7ddb9053ffec6a2f8343afc659984
David2999999 <djiang86@binghamton.edu>
2019-09-10 09:11:50 -0400

    started angular indept

--- Sports-Store/src/app/model/datasource/rest.datasource.ts
+++ Sports-Store/src/app/model/datasource/rest.datasource.ts
@@ -14,7 +14,8 @@ export class RestDataSource {
   auth_token: string;
 
   constructor(private http: HttpClient) {
-    this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
+    //this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
+    this.baseUrl = "/api/"
   }
 
   getProducts(): Observable<Product[]> {
