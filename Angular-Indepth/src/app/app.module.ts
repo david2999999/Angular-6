ee71a533aba7f7619535fbfe574e3ec7f3068810
David2999999 <djiang86@binghamton.edu>
2019-09-11 08:41:47 -0400

    adding components into modules

--- Angular-Indepth/src/app/app.module.ts
+++ Angular-Indepth/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ProductComponent} from "./component/component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ProductComponent
   ],
   imports: [
     BrowserModule
