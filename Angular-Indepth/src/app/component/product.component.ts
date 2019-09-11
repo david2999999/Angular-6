c69b57647c45d1de94ef01753a415636e85fdee9
David2999999 <djiang86@binghamton.edu>
2019-09-11 14:46:33 -0400

    set element classes with standard binding

--- Angular-Indepth/src/app/component/product.component.ts
+++ Angular-Indepth/src/app/component/product.component.ts
@@ -8,7 +8,8 @@ import {Model} from "../model/repository.model";
 export class ProductComponent {
   model: Model = new Model();
 
-  getClasses(): string {
-    return this.model.getProducts().length == 5 ? "bg-success" : "bg-warning";
+  getClasses(key: number): string {
+    let product = this.model.getProduct(key);
+    return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
   }
 }
