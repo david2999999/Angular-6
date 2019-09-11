32c608b179d59b9d733e16cb00a6b157c60c9e61
David2999999 <djiang86@binghamton.edu>
2019-09-11 11:28:55 -0400

    angular property binding

--- Angular-Indepth/src/app/app.module.ts
+++ Angular-Indepth/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {ProductComponent} from "./component/component";
+import {ProductComponent} from "./component/product.component";
 
 @NgModule({
   declarations: [
--- Angular-Indepth/src/app/component/product.component.ts
+++ Angular-Indepth/src/app/component/product.component.ts
@@ -3,8 +3,12 @@ import {Model} from "../model/repository.model";
 
 @Component({
   selector: "app",
-  templateUrl: "template.html"
+  templateUrl: "product.component.html"
 })
 export class ProductComponent {
   model: Model = new Model();
+
+  getClasses(): string {
+    return this.model.getProducts().length == 5 ? "bg-success" : "bg-warning";
+  }
 }
