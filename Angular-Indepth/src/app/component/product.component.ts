af02ca9e19d40b24dfacbdde6d011be7ca98ad2c
David2999999 <djiang86@binghamton.edu>
2019-09-11 15:11:47 -0400

    setting classes using the ngClass Directive

--- Angular-Indepth/src/app/component/product.component.ts
+++ Angular-Indepth/src/app/component/product.component.ts
@@ -12,4 +12,12 @@ export class ProductComponent {
     let product = this.model.getProduct(key);
     return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
   }
+
+  getClassMap(key: number): Object {
+    let product = this.model.getProduct(key);
+    return {
+      "text-center bg-danger": product.name == "Kayak",
+      "bg-info": product.price < 50
+    };
+  }
 }
