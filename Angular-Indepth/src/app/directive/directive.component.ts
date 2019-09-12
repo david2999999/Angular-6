d62dd11200a9a3d5f2a0ad6747a54891454c5976
David2999999 <djiang86@binghamton.edu>
2019-09-12 08:44:39 -0400

    avoiding literal value problems in ngSwitch

--- Angular-Indepth/src/app/directive/directive.component.ts
+++ Angular-Indepth/src/app/directive/directive.component.ts
@@ -18,15 +18,18 @@ export class DirectiveComponent {
   getProductByPosition(position: number): Product {
     return this.model.getProducts()[position];
   }
+
   getProduct(key: number): Product {
     return this.model.getProduct(key);
   }
+
   getProducts(): Product[] {
     return this.model.getProducts();
   }
+
   getProductCount(): number {
     return this.getProducts().length;
   }
-  targetName: string = "Kayak";
 
+  targetName: string = "Kayak";
 }
