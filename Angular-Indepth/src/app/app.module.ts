807f521f05b2cede57e07c6757fe2f896fd37f06
David2999999 <djiang86@binghamton.edu>
2019-09-13 13:59:10 -0400

    model-based form validation

--- Angular-Indepth/src/app/app.module.ts
+++ Angular-Indepth/src/app/app.module.ts
@@ -4,16 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {ProductComponent} from "./component/product.component";
 import { DirectiveComponent } from './directive/directive.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { FormComponent } from './form/form.component';
 import { ProductFormComponent } from './product-form/product-form.component';
+import { ModelBasedFormComponent } from './model-based-form/model-based-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ProductComponent, DirectiveComponent, FormComponent, ProductFormComponent
+    AppComponent, ProductComponent, DirectiveComponent, FormComponent, ProductFormComponent, ModelBasedFormComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Angular-Indepth/src/app/model-based-form/model-based-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-model-based-form',
+  templateUrl: './model-based-form.component.html',
+  styleUrls: ['./model-based-form.component.css']
+})
+export class ModelBasedFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
