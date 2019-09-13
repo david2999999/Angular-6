ec15056a624a6629c108f7da593d4877b485c73b
David2999999 <djiang86@binghamton.edu>
2019-09-13 14:09:15 -0400

    validation error messages in model-based validaiton

--- Angular-Indepth/src/app/model/product-form.model.ts
+++ Angular-Indepth/src/app/model/product-form.model.ts
@@ -3,10 +3,39 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
 export class ProductFormControl extends FormControl {
   label: string;
   modelProperty: string;
+
   constructor(label:string, property:string, value: any, validator: any) {
     super(value, validator);
     this.label = label;
     this.modelProperty = property;
+  }
+
+  getValidationMessages() {
+    let messages: string[] = [];
+
+    if (this.errors) {
+      for (let errorName in this.errors) {
+        switch (errorName) {
+          case "required":
+            messages.push(`You must enter a ${this.label}`);
+            break;
+          case "minlength":
+            messages.push(`A ${this.label} must be at least
+                          ${this.errors['minlength'].requiredLength}
+                          characters`);
+            break;
+          case "maxlength":
+            messages.push(`A ${this.label} must be no more than
+                          ${this.errors['maxlength'].requiredLength}
+                          characters`);
+            break;
+          case "pattern":
+            messages.push(`The ${this.label} contains illegal characters`);
+            break;
+        }
+      }
+    }
+    return messages;
   }
 }
 
@@ -30,5 +59,12 @@ export class ProductFormGroup extends FormGroup {
   get productControls(): ProductFormControl[] {
     return Object.keys(this.controls)
       .map(k => this.controls[k] as ProductFormControl);
+  }
+
+  getFormValidationMessages(form: any) : string[] {
+    let messages: string[] = [];
+    this.productControls.forEach(c => c.getValidationMessages()
+      .forEach(m => messages.push(m)));
+    return messages;
   }
 }
