eb343a30398e8d55bb713b9979fe3ff23a7e3866
David2999999 <djiang86@binghamton.edu>
2019-09-13 14:40:23 -0400

    binding model based form validation to form

--- Angular-Indepth/src/app/model-based-form/model-based-form.component.ts
+++ Angular-Indepth/src/app/model-based-form/model-based-form.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from "../model/product.model";
+import {NgForm} from "@angular/forms";
+import {ProductFormGroup} from "../model/product-form.model";
+import {Model} from "../model/repository.model";
 
 @Component({
   selector: 'app-model-based-form',
   templateUrl: './model-based-form.component.html',
   styleUrls: ['./model-based-form.component.css']
 })
-export class ModelBasedFormComponent implements OnInit {
+export class ModelBasedFormComponent {
+  model: Model = new Model();
+  form: ProductFormGroup = new ProductFormGroup();
 
-  constructor() { }
-
-  ngOnInit() {
+  getProduct(key: number): Product {
+    return this.model.getProduct(key);
   }
 
+  getProducts(): Product[] {
+    return this.model.getProducts();
+  }
+
+  newProduct: Product = new Product();
+
+  get jsonProduct() {
+    return JSON.stringify(this.newProduct);
+  }
+
+  addProduct(p: Product) {
+    console.log("New Product: " + this.jsonProduct);
+  }
+
+  formSubmitted: boolean = false;
+
+  submitForm(form: NgForm) {
+    this.formSubmitted = true;
+    if (form.valid) {
+      this.addProduct(this.newProduct);
+      this.newProduct = new Product();
+      form.reset();
+      this.formSubmitted = false;
+    }
+  }
 }
