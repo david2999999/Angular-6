577a361ebb2a0f11155daa88a1b7c938973596ed
David2999999 <djiang86@binghamton.edu>
2019-09-17 08:21:16 -0400

    custom directives

---
+++ Angular-Indepth/src/app/attribute-directives/directives/attr.directive.ts
@@ -0,0 +1,10 @@
+import {Directive, ElementRef} from "@angular/core";
+
+@Directive({
+  selector: "[pa-attr]",
+})
+export class PaAttrDirective {
+  constructor(element: ElementRef) {
+    element.nativeElement.classList.add("bg-success", "text-white");
+  }
+}
