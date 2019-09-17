0d019e43aca6a7d0479f78c06b9cf2a25b3aa940
David2999999 <djiang86@binghamton.edu>
2019-09-17 14:30:45 -0400

    using data bound input properties to directives

--- Angular-Indepth/src/app/attribute-directives/directives/attr.directive.ts
+++ Angular-Indepth/src/app/attribute-directives/directives/attr.directive.ts
@@ -1,10 +1,16 @@
-import {Attribute, Directive, ElementRef} from "@angular/core";
+import {Attribute, Directive, ElementRef, Input} from "@angular/core";
 
 @Directive({
   selector: "[pa-attr]",
 })
 export class PaAttrDirective {
-  constructor(element: ElementRef, @Attribute("pa-attr-class") bgClass: string) {
-    element.nativeElement.classList.add(bgClass || "bg-success", "text-white");
+  constructor(private element: ElementRef) {}
+
+  @Input("pa-attr")
+  bgClass: string;
+
+  ngOnInit() {
+    this.element.nativeElement.classList.add(this.bgClass || "bg-success",
+      "text-white");
   }
 }
