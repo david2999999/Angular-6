7c8a8ff9a3d73c5c12784a13595beb550d7770af
David2999999 <djiang86@binghamton.edu>
2019-09-17 14:19:24 -0400

    adding attributes to angular directives

--- Angular-Indepth/src/app/attribute-directives/directives/attr.directive.ts
+++ Angular-Indepth/src/app/attribute-directives/directives/attr.directive.ts
@@ -1,10 +1,10 @@
-import {Directive, ElementRef} from "@angular/core";
+import {Attribute, Directive, ElementRef} from "@angular/core";
 
 @Directive({
   selector: "[pa-attr]",
 })
 export class PaAttrDirective {
-  constructor(element: ElementRef) {
-    element.nativeElement.classList.add("bg-success", "text-white");
+  constructor(element: ElementRef, @Attribute("pa-attr-class") bgClass: string) {
+    element.nativeElement.classList.add(bgClass || "bg-success", "text-white");
   }
 }
