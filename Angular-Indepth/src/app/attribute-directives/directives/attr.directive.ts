1996ff80295097496f72bc65e89f0a1907ca1870
David2999999 <djiang86@binghamton.edu>
2019-09-18 08:03:23 -0400

    using host element binding

--- Angular-Indepth/src/app/attribute-directives/directives/attr.directive.ts
+++ Angular-Indepth/src/app/attribute-directives/directives/attr.directive.ts
@@ -1,19 +1,19 @@
-import {Attribute, Directive, ElementRef, EventEmitter, Input, Output, SimpleChange} from "@angular/core";
+import {
+  Directive,
+  EventEmitter,
+  HostBinding,
+  HostListener,
+  Input,
+  Output,
+} from "@angular/core";
 import {Product} from "../../model/product.model";
 
 @Directive({
   selector: "[pa-attr]",
 })
 export class PaAttrDirective {
-  constructor(private element: ElementRef) {
-    this.element.nativeElement.addEventListener("click", e => {
-      if (this.product != null) {
-        this.click.emit(this.product.category);
-      }
-    });
-  }
-
   @Input("pa-attr")
+  @HostBinding("class")
   bgClass: string;
 
   @Input("pa-product")
@@ -22,21 +22,10 @@ export class PaAttrDirective {
   @Output("pa-category")
   click = new EventEmitter<string>();
 
-  ngOnInit() {
-    this.element.nativeElement.classList.add(this.bgClass || "bg-success",
-      "text-white");
-  }
-
-  ngOnChanges(changes: {[property: string]: SimpleChange }) {
-    let change = changes["bgClass"];
-    let classList = this.element.nativeElement.classList;
-
-    if (!change.isFirstChange() && classList.contains(change.previousValue)) {
-      classList.remove(change.previousValue);
-    }
-
-    if (!classList.contains(change.currentValue)) {
-      classList.add(change.currentValue);
+  @HostListener("click")
+  triggerCustomEvent() {
+    if (this.product != null) {
+      this.click.emit(this.product.category);
     }
   }
 }
