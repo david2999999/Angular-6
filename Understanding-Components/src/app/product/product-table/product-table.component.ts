e760b3a89c835e2a363c4a260304042915038dfb
David2999999 <djiang86@binghamton.edu>
2019-09-20 11:39:18 -0400

    creating new project for understanding components

---
+++ Understanding-Components/src/app/product/product-table/product-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'paProductTable',
+  templateUrl: './product-table.component.html',
+  styleUrls: ['./product-table.component.css']
+})
+export class ProductTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
