4b624174316f032eaa73c27c80bc7bbf6f7f30ee
david2999999 <djiang86@binghamton.edu>
2019-10-23 15:22:34 -0400

    date pipe

--- Understanding-Components/src/app/product/product-table/product-table.component.ts
+++ Understanding-Components/src/app/product/product-table/product-table.component.ts
@@ -24,4 +24,8 @@ export class ProductTableComponent {
   deleteProduct(key: number) {
     this.dataModel.deleteProduct(key);
   }
+
+  dateObject: Date = new Date(2020, 1, 20);
+  dateString: string = "2020-02-20T00:00:00.000Z";
+  dateNumber: number = 1582156800000;
 }
