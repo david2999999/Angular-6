eb3c4897e2d47c2b39eb200cdcaf6729e07561eb
david2999999 <djiang86@binghamton.edu>
2019-11-01 08:03:18 -0400

    creating table component

---
+++ Advanced-Angular-Features/src/app/core/table/table.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {Model} from "../../model/repository.model";
+import {MODES, SharedState} from "../sharedState.model";
+import {Product} from "../../model/product.model";
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent {
+
+  constructor(private model: Model, private state: SharedState) { }
+
+  getProduct(key: number): Product {
+    return this.model.getProduct(key);
+  }
+
+  getProducts(): Product[] {
+    return this.model.getProducts();
+  }
+
+  deleteProduct(key: number) {
+    this.model.deleteProduct(key);
+  }
+
+  editProduct(key: number) {
+    this.state.id = key;
+    this.state.mode = MODES.EDIT;
+  }
+
+  createProduct() {
+    this.state.id = undefined;
+    this.state.mode = MODES.CREATE;
+  }
+}
---
+++ Advanced-Angular-Features/src/app/model/model.module.ts
@@ -0,0 +1,8 @@
+import {NgModule} from "@angular/core";
+import {Model} from "./repository.model";
+import {StaticDataSource} from "./static.datasource";
+
+@NgModule({
+  providers: [Model, StaticDataSource]
+})
+export class ModelModule { }
