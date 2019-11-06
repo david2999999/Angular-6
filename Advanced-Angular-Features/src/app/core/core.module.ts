b1fcff63218ff8d7df788c6819826f7c0b4f0da2
david2999999 <djiang86@binghamton.edu>
2019-11-06 08:57:02 -0500

    using subject for observable and observer

--- Advanced-Angular-Features/src/app/core/core.module.ts
+++ Advanced-Angular-Features/src/app/core/core.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { ModelModule } from "../model/model.module";
-import { SharedState } from "./sharedState.model";
+import {SHARED_STATE, SharedState} from "./sharedState.model";
 import {TableComponent} from "./table/table.component";
 import {FormComponent} from "./form/form.component";
+import {Subject} from "rxjs";
 
 @NgModule({
   imports: [BrowserModule, FormsModule, ModelModule],
   declarations: [TableComponent, FormComponent],
   exports: [ModelModule, TableComponent, FormComponent],
-  providers: [SharedState]
-})
+  providers: [{ provide: SHARED_STATE, useValue: new Subject<SharedState>() }]})
 export class CoreModule { }
