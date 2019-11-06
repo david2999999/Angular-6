309b298b2c7d0a1b55be28cae294b6f6608a76c3
david2999999 <djiang86@binghamton.edu>
2019-11-06 08:37:54 -0500

    using Observer to send out events

--- Advanced-Angular-Features/src/app/core/table/table.component.ts
+++ Advanced-Angular-Features/src/app/core/table/table.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {Model} from "../../model/repository.model";
-import {MODES, SharedState} from "../sharedState.model";
+import {MODES, SHARED_STATE, SharedState} from "../sharedState.model";
 import {Product} from "../../model/product.model";
+import {Observer} from "rxjs";
 
 @Component({
   selector: 'app-table',
@@ -9,8 +10,8 @@ import {Product} from "../../model/product.model";
   styleUrls: ['./table.component.css']
 })
 export class TableComponent {
-
-  constructor(private model: Model, private state: SharedState) { }
+  constructor(private model: Model,
+              @Inject(SHARED_STATE) private observer: Observer<SharedState>) { }
 
   getProduct(key: number): Product {
     return this.model.getProduct(key);
@@ -25,12 +26,10 @@ export class TableComponent {
   }
 
   editProduct(key: number) {
-    this.state.id = key;
-    this.state.mode = MODES.EDIT;
+    this.observer.next(new SharedState(MODES.EDIT, key));
   }
 
   createProduct() {
-    this.state.id = undefined;
-    this.state.mode = MODES.CREATE;
+    this.observer.next(new SharedState(MODES.CREATE));
   }
 }
