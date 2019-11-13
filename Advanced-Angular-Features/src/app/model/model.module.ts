0d768e6cc4a435bb80a6d8d14888d7a8b3c720c1
David <djiang@smlny.com>
2019-11-13 08:17:51 -0500

    setting up JSON server for http requests

--- Advanced-Angular-Features/src/app/model/model.module.ts
+++ Advanced-Angular-Features/src/app/model/model.module.ts
@@ -1,8 +1,10 @@
 import {NgModule} from "@angular/core";
 import {Model} from "./repository.model";
 import {StaticDataSource} from "./static.datasource";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
+  imports: [HttpClientModule],
   providers: [Model, StaticDataSource]
 })
 export class ModelModule { }
