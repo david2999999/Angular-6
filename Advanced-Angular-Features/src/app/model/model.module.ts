a36e2113cd66105fac6ba5235100cc0a4ef1937f
david2999999 <djiang86@binghamton.edu>
2019-11-13 09:06:10 -0500

    register rest datasource and URL

--- Advanced-Angular-Features/src/app/model/model.module.ts
+++ Advanced-Angular-Features/src/app/model/model.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from "@angular/core";
 import {Model} from "./repository.model";
-import {StaticDataSource} from "./static.datasource";
 import {HttpClientModule} from "@angular/common/http";
+import {REST_URL, RestDataSource} from "./rest.datasource";
 
 @NgModule({
   imports: [HttpClientModule],
-  providers: [Model, StaticDataSource]
+  providers: [Model, RestDataSource,
+    { provide: REST_URL, useValue: `http://${location.hostname}:3500/products` }]
 })
 export class ModelModule { }
