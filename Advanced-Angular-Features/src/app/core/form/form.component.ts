fb15ce85fa054f33a9c9e5042243594920ad8582
David <djiang@smlny.com>
2019-11-13 08:17:51 -0500

    setting up JSON server for http requests

--- Advanced-Angular-Features/src/app/core/form/form.component.ts
+++ Advanced-Angular-Features/src/app/core/form/form.component.ts
@@ -17,9 +17,9 @@ export class FormComponent {
   constructor(private model: Model,
               @Inject(SHARED_STATE) private stateEvents: Observable<SharedState>) {
     stateEvents
-      .pipe(skipWhile(state => state.mode == MODES.EDIT))
-      .pipe(distinctUntilChanged((firstState, secondState) =>
-        firstState.mode == secondState.mode && firstState.id == secondState.id))
+      // .pipe(skipWhile(state => state.mode == MODES.EDIT))
+      // .pipe(distinctUntilChanged((firstState, secondState) =>
+      //   firstState.mode == secondState.mode && firstState.id == secondState.id))
       .subscribe(update => {
         this.product = new Product();
         if (update.id != undefined) {
