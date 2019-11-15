3952f6def553d1303ea0e89aae33fa64de65c356
david2999999 <djiang86@binghamton.edu>
2019-11-15 08:49:45 -0500

    adding more CRUD operations to restful service

--- Advanced-Angular-Features/src/app/model/rest.datasource.ts
+++ Advanced-Angular-Features/src/app/model/rest.datasource.ts
@@ -7,9 +7,23 @@ export const REST_URL = new InjectionToken("rest_url");
 
 @Injectable()
 export class RestDataSource {
+
   constructor(private http: HttpClient,
               @Inject(REST_URL) private url: string) { }
+
   getData(): Observable<Product[]> {
     return this.http.get<Product[]>(this.url);
+  }
+
+  saveProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.url, product);
+  }
+
+  updateProduct(product: Product): Observable<Product> {
+    return this.http.put<Product>(`${this.url}/${product.id}`, product);
+  }
+
+  deleteProduct(id: number): Observable<Product> {
+    return this.http.delete<Product>(`${this.url}/${id}`);
   }
 }
