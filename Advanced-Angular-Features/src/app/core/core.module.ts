69257ef4182390a6e85d3cadfde66fce65a94aff
david2999999 <djiang86@binghamton.edu>
2019-11-18 08:59:48 -0500

    using routerLink to navigate between views

--- Advanced-Angular-Features/src/app/core/core.module.ts
+++ Advanced-Angular-Features/src/app/core/core.module.ts
@@ -10,10 +10,10 @@ import {StatePipe} from "./pipe/state.pipe";
 import {MessageModule} from "../messages/message.module";
 import {MessageService} from "../messages/service/message.service";
 import {Model} from "../model/repository.model";
-import {Message} from "../messages/message.model";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ModelModule, MessageModule],
+  imports: [BrowserModule, FormsModule, ModelModule, MessageModule, RouterModule],
   declarations: [TableComponent, FormComponent, StatePipe],
   exports: [ModelModule, TableComponent, FormComponent],
   providers: [{
