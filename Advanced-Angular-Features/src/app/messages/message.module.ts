dd394c78602b564cbe4ed60ef6910ca76919467a
david2999999 <djiang86@binghamton.edu>
2019-11-21 08:06:27 -0500

    receiving navigation events

--- Advanced-Angular-Features/src/app/messages/message.module.ts
+++ Advanced-Angular-Features/src/app/messages/message.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import {MessageComponent} from "./message/message.component";
 import {MessageService} from "./service/message.service";
 import {MessageErrorHandler} from "./errorHandler";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule],
   declarations: [MessageComponent],
   exports: [MessageComponent],
   providers: [MessageService,
--- Advanced-Angular-Features/src/app/messages/message/message.component.ts
+++ Advanced-Angular-Features/src/app/messages/message/message.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Message} from "../message.model";
 import {MessageService} from "../service/message.service";
+import {NavigationCancel, NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs/operators";
 
 @Component({
   selector: 'message',
@@ -10,7 +12,11 @@ import {MessageService} from "../service/message.service";
 export class MessageComponent {
   lastMessage: Message;
 
-  constructor(messageService: MessageService) {
+  constructor(messageService: MessageService, router: Router) {
     messageService.messages.subscribe(m => this.lastMessage = m);
+
+    router.events
+      .pipe(filter(e => e instanceof NavigationEnd || e instanceof NavigationCancel))
+      .subscribe(e => { this.lastMessage = null; });
   }
 }
