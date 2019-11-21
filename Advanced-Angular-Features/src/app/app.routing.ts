2bde4b4eba693fe4a0b3b63bbbd8c28d1a8e543c
david2999999 <djiang86@binghamton.edu>
2019-11-21 09:13:43 -0500

    using wildcard routes

--- Advanced-Angular-Features/src/app/app.routing.ts
+++ Advanced-Angular-Features/src/app/app.routing.ts
@@ -1,10 +1,13 @@
 import { Routes, RouterModule } from "@angular/router";
 import {FormComponent} from "./core/form/form.component";
 import {TableComponent} from "./core/table/table.component";
+import {NotFoundComponent} from "./core/notFound.component";
 
 const routes: Routes = [
   { path: "form/:mode/:id", component: FormComponent },
   { path: "form/:mode", component: FormComponent },
-  { path: "", component: TableComponent }];
+  { path: "", component: TableComponent },
+  { path: "**", component: NotFoundComponent }
+];
 
 export const routing = RouterModule.forRoot(routes);
--- Advanced-Angular-Features/src/app/core/notFound.component.ts
+++ Advanced-Angular-Features/src/app/core/notFound.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "paNotFound",
-  template: `<h3 class="bg-danger text-white p-2">Sorry, something went wrong</h3>
+  template: `<h3 class="bg-danger text-white p-2" style="text-align: center;">Sorry, something went wrong</h3>
             <button class="btn btn-primary" routerLink="/">Start Over</button>`
 })
 export class NotFoundComponent {}
