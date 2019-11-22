de2eb3402a0879969ede703ab286b851c1b02d4b
david2999999 <djiang86@binghamton.edu>
2019-11-22 09:39:19 -0500

    child routes

--- Advanced-Angular-Features/src/app/app.routing.ts
+++ Advanced-Angular-Features/src/app/app.routing.ts
@@ -2,13 +2,22 @@ import { Routes, RouterModule } from "@angular/router";
 import {FormComponent} from "./core/form/form.component";
 import {TableComponent} from "./core/table/table.component";
 import {NotFoundComponent} from "./core/notFound.component";
+import {ProductCountComponent} from "./core/productCount.component";
+import {CategoryCountComponent} from "./core/categoryCount.component";
 
 const routes: Routes = [
   { path: "form/:mode/:id", component: FormComponent },
   { path: "form/:mode", component: FormComponent },
   { path: "does", redirectTo: "/form/create", pathMatch: "prefix" },
+  {
+    path: "table",
+    component: TableComponent,
+    children: [
+      { path: "products", component: ProductCountComponent },
+      { path: "categories", component: CategoryCountComponent }
+    ]
+  },
   { path: "table/:category", component: TableComponent },
-  { path: "table", component: TableComponent },
   { path: "", redirectTo: "/table", pathMatch: "full" },
   { path: "**", component: NotFoundComponent }
 ];
