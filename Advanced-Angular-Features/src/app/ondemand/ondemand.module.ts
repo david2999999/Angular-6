ae4d92003596dd545fbb83a46d408e6fcf359600
david2999999 <djiang86@binghamton.edu>
2019-12-04 08:08:59 -0500

    creating a module for lazy loading

---
+++ Advanced-Angular-Features/src/app/ondemand/ondemand.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { OndemandComponent } from "./ondemand.component";
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [OndemandComponent],
+  exports: [OndemandComponent]
+})
+export class OndemandModule { }
