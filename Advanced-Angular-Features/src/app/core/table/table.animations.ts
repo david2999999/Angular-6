2b409cb3bb6bc2db7d160bf0e7c00551b769a299
david2999999 <djiang86@binghamton.edu>
2019-12-04 14:10:40 -0500

    parallel animations

--- Advanced-Angular-Features/src/app/core/table/table.animations.ts
+++ Advanced-Angular-Features/src/app/core/table/table.animations.ts
@@ -1,4 +1,4 @@
-import { trigger, style, state, transition, animate } from "@angular/animations";
+import {trigger, style, state, transition, animate, group} from "@angular/animations";
 
 export const HighlightTrigger = trigger("rowHighlight", [
   state("selected", style({
@@ -13,6 +13,21 @@ export const HighlightTrigger = trigger("rowHighlight", [
     opacity: 0
   })),
   transition("* => notselected", animate("200ms")),
-  transition("* => selected", animate("400ms ease-in")),
+  transition("* => selected",
+    [animate("400ms 200ms ease-in",
+      style({
+        backgroundColor: "lightblue",
+        fontSize: "25px"
+      })),
+      group([
+        animate("250ms", style({
+          backgroundColor: "lightcoral",
+        })),
+        animate("450ms", style({
+          fontSize: "30px"
+        })),
+      ]),
+      animate("200ms")]
+  ),
   transition("void => *", animate("500ms"))
 ]);
