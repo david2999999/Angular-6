8a50ce99f836b574862aaadcb51561dc48e98641
david2999999 <djiang86@binghamton.edu>
2019-12-04 09:07:45 -0500

    using multiple router outlets

--- Advanced-Angular-Features/src/app/ondemand/ondemand.module.ts
+++ Advanced-Angular-Features/src/app/ondemand/ondemand.module.ts
@@ -2,14 +2,25 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { OndemandComponent } from "./ondemand.component";
 import {RouterModule} from "@angular/router";
+import {FirstComponent} from "./first.component";
+import {SecondComponent} from "./second.component";
 
 let routing = RouterModule.forChild([
-  { path: "", component: OndemandComponent }
+  { path: "", component: OndemandComponent,
+    children: [
+      { path: "",
+        children: [
+          { outlet: "primary", path: "", component: FirstComponent, },
+          { outlet: "left", path: "", component: SecondComponent, },
+          { outlet: "right", path: "", component: SecondComponent, },
+        ]},
+      ]
+  }
 ]);
 
 @NgModule({
   imports: [CommonModule, routing],
-  declarations: [OndemandComponent],
+  declarations: [OndemandComponent, FirstComponent, SecondComponent],
   exports: [OndemandComponent]
 })
 export class OndemandModule { }
